d3ed6783fa91ba84e2c52f32749c413bf5192a50
riya shah <71328725+riyaashah29@users.noreply.github.com>
2023-04-08 15:25:11 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -196,7 +196,7 @@ exports.postReset = (req,res,next) => {
               subject: 'Password Reset',
               html : `
                 <p>You requested a Password Reset</p>
-                <p>Click this <a href="http://localhost:4000/reset/${token}"> link </a> to set a new password/</p>
+//                 <p>Click this <a href="http://localhost:4000/reset/${token}"> link </a> to set a new password/</p>
               `
               }, err =>{
               console.log(err);
